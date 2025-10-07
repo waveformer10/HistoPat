c956f238ee84a2dcbf3f2b98b5d418cc3949d9a9
Gabriel Lucena <gabriel.lucena@b2list.com>
2025-10-07 16:06:02 -0300

    Iniciando estilização base

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -2,7 +2,7 @@ import { tv } from "tailwind-variants";
 import { InputProps } from "./Input.types";
 
 const inputStyles = tv({
-  base: "",
+  base: "border-light-gray flex h-56 w-lg items-center justify-items-start border-2",
   variants: {
     color: {
       primary: "",
