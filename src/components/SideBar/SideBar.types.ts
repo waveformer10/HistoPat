e2aaafc955ff8eda3dfc5897192d621f98b39ae7
Ericbsl <ericbsl@unipam.edu.br>
2025-10-17 17:23:36 -0300

    componente SideBar

---
+++ src/components/SideBar/SideBar.types.ts
@@ -0,0 +1,8 @@
+export type SideBarProps = {
+    children?: React.ReactNode,
+    image?: string,
+    collapsible?: boolean,
+    colorNavigation?: "primary" | "secondary",
+    title?: string,
+    colorText?: "black" | "white"
+}
