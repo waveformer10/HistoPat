f41f07bb6cbb423d11c22bc76e1a65afb997c2e9
danilo-oliv <daniloborges@unipam.edu.br>
2025-10-21 11:29:31 -0300

    feat: componente lista de adms

---
+++ src/components/ListCard/ListCard.tsx
@@ -0,0 +1,52 @@
+import { ButtonProps } from "../Button/Button.types";
+import { IconLib } from "../IconLib/IconLib";
+import { ListCardProps } from "./ListCard.types";
+
+
+export function ListCard(props: ListCardProps) {
+  return (
+    <div
+      className="
+        grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5
+        gap-6 md:gap-10
+        outline outline-gray-400 rounded-lg
+        font-medium text-gray-900 m-4 py-6 px-4
+        min-h-18
+      "
+    >
+      <div className="flex justify-center items-center">
+        <p className="bg-dark-blue rounded-full w-14 h-14 flex items-center justify-center text-white text-lg">
+          {props.username.at(0)}
+        </p>
+      </div>
+
+      <p className="flex items-center justify-center text-center truncate">{props.username}</p>
+      <p className="flex items-center justify-center text-center">{props.userRole}</p>
+
+      <div className="flex justify-center items-center">
+        <button className="px-3 py-2">
+          <IconLib
+            iconLibName="lu"
+            icon="LuPencil"
+            color="var(--color-black)"
+            fill="var(--color-white)"
+            size={23}
+          />
+        </button>
+      </div>
+
+      <div className="flex justify-center items-center">
+        <button className="px-3 py-2">
+          <IconLib
+            iconLibName="hi2"
+            icon="HiOutlineTrash"
+            color="var(--color-black)"
+            fill="var(--color-white)"
+            size={23}
+          />
+        </button>
+      </div>
+    </div>
+  );
+}
+
