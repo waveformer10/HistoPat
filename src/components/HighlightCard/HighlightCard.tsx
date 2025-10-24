6c158db64ddb404a06a023578bb48bbe1b1fde89
danilo-oliv <daniloborges@unipam.edu.br>
2025-10-24 10:21:44 -0300

    feat: criação do componente do dashboard

---
+++ src/components/HighlightCard/HighlightCard.tsx
@@ -0,0 +1,10 @@
+
+
+export function HighlightCard(){
+  return (
+    <div className="bg-[#26406C] place-items-center place-content-center w-fit !p-6 rounded">
+      <p className="!p-2">Total de Lâminas cadastradas</p>
+      <h1 className={"text-4xl !p-2 hover:transform hover:scale-125"}>1.080</h1>
+    </div>
+  )
+}
