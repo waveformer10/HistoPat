b6532b092d43b13827cefcbda4f82a8dbed7d3b3
lauanegs <lauanegonzagasilva@gmail.com>
2025-11-11 17:46:01 -0300

    adicionando partes do microscópio

--- src/components/Microscope/Microscope.tsx
+++ src/components/Microscope/Microscope.tsx
@@ -10,29 +10,93 @@ type PartInfo = {
 
 const PARTS: { id: string; area: { top: number; left: number; width: number; height: number }; info: PartInfo }[] = [
   {
-    id: "base",
+    id: "PÉ OU BASE",
     area: { top: 320, left: 20, width: 160, height: 80 },
     info: {
-      name: "Base",
-      description: "Sustenta o microscópio e garante sua estabilidade."
+      name: "PÉ OU BASE",
+      description: "Sustenta demais peças."
     }
   },
   {
-    id: "focus",
+    id: "PARAFUSOS MACROMÉTRICO E MICROMÉTRICO",
     area: { top: 200, left: 180, width: 80, height: 80 },
     info: {
-      name: "Parafuso de foco",
-      description: "Permite ajustar o foco da imagem."
+      name: "PARAFUSOS MACROMÉTRICO E MICROMÉTRICO",
+      description: "Macrométrico: Permite regular a altura da platina, através de movimentos amplos para um ajuste grosso do foco. Micrométrico: Permite regular a altura da platina, porém é um ajuste fino do foco."
     }
   },
   {
-    id: "ocular",
+    id: "CHARRIOT",
     area: { top: 20, left: 80, width: 100, height: 120 },
     info: {
-      name: "Ocular",
-      description: "Local onde o observador posiciona os olhos."
+      name: "CHARRIOT",
+      description: "Sua função é permitir o movimento da lâmina sobre a platina."
     }
   }
+  {
+    id: "PLATINA",
+    area: { top: 320, left: 20, width: 160, height: 80 },
+    info: {
+      name: "PLATINA",
+      description: "Plataforma plana que tem como função suportar o material ou lâmina que está em observação. "
+    }
+  },
+  {
+    id: "BRAÇO OU COLUNA",
+    area: { top: 200, left: 180, width: 80, height: 80 },
+    info: {
+      name: "BRAÇO OU COLUNA",
+      description: "É fixo na base do microscópio e serve de suporte para as demais partes."
+    }
+  },
+  {
+    id: "REVOLVER",
+    area: { top: 20, left: 80, width: 100, height: 120 },
+    info: {
+      name: "REVOLVER",
+      description: "Utensílio giratório que acopla as lentes objetivas, modificando o aumento de acordo com o giro."
+    }
+  },
+  {
+    id: "TUBO ÓTICO OU CANHÃO",
+    area: { top: 320, left: 20, width: 160, height: 80 },
+    info: {
+      name: "TUBO ÓTICO OU CANHÃO",
+      description: "É a estrutura responsável por sustentar as lentes oculares."
+    }
+  },
+  {
+    id: "FONTE DE LUZ",
+    area: { top: 200, left: 180, width: 80, height: 80 },
+    info: {
+      name: "FONTE DE LUZ",
+      description: "Responsável pela iluminação que permite a observação ideal de suas amostras, bem como a captura de imagens e análise de dados."
+    }
+  },
+  {
+    id: "CONDENSADOR E DIAFRAGMA",
+    area: { top: 20, left: 80, width: 100, height: 120 },
+    info: {
+      name: "CONDENSADOR E DIAFRAGMA",
+      description: "Diafragma: controla tamanho e intensidade do cone de luz que é projetado sobre o objeto. Condensador: controla o foco e posicionamento da luz sobre a amostra analisada. "
+    }
+  },
+  {
+    id: "LENTE OBJETIVA",
+    area: { top: 320, left: 20, width: 160, height: 80 },
+    info: {
+      name: "LENTE OBJETIVA",
+      description: "É a lente que fica mais próxima da lâmina e forma uma imagem real do objeto. Apresenta aumentos de: 40x, 100x, 400x e 1000x (necessário óleo de imerção)."
+    }
+  },
+  {
+    id: "LENTE OCULAR",
+    area: { top: 320, left: 20, width: 160, height: 80 },
+    info: {
+      name: "LENTE OCULAR",
+      description: "É a lente que fica mais próxima do olho e funciona como uma lente de aumento para observar a imagem formada pela objetiva. "
+    }
+  },
 ];
 
 export default function MicroscopeInteractive() {
